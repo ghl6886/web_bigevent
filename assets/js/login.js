74ede9b9eecff71dad4d2a7a99933d6a26e28f6c
guo <3086794898@qq.com>
2020-09-30 14:52:30 +0800

    完成了登录和注册

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,20 +1,19 @@
-$(function() {
-  // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
-    $('.login-box').hide()
-    $('.reg-box').show()
+$(function(){
+  // 点击去注册账号的链接
+  $('#link_reg').on('click',function(){
+    $('.login-box').hide();
+    $('.reg-box').show();
   })
 
-  // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
-    $('.login-box').show()
-    $('.reg-box').hide()
+  // 点击去登陆链接
+  $('#link_login').on('click',function(){
+    $('.reg-box').hide();
+    $('.login-box').show();
   })
-
-  // 从 layui 中获取 form 对象
+  // 从layui中获取form对象
   var form = layui.form
   var layer = layui.layer
-  // 通过 form.verify() 函数自定义校验规则
+  // 通过form.verify()函数自定义校验规则
   form.verify({
     // 自定义了一个叫做 pwd 校验规则
     pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
@@ -50,25 +49,26 @@ $(function() {
     })
   })
 
-  // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
-    // 阻止默认提交行为
+// 监听登录表单事件
+$('#form_login').submit(function(e){
+    // 1. 阻止默认的提交行为
     e.preventDefault()
     $.ajax({
-      url: '/api/login',
-      method: 'POST',
-      // 快速获取表单中的数据
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
+      url:'/api/login',
+      method:'POST',
+      data:$(this).serialize(),//快速获取表单中的数据
+      success:function(res){
+        if(res.status !== 0){
           return layer.msg('登录失败！')
         }
-        layer.msg('登录成功！')
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
+        layer.msg('登录成功')
+
+        // 将登录成功得到的token字符串，保存到localStorage中
         localStorage.setItem('token', res.token)
         // 跳转到后台主页
         location.href = '/index.html'
       }
     })
-  })
+})
+  
 })
